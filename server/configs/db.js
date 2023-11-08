ebbb639ee46a4cfeb2fe47b4e048cd14ca7b176d
Jayasankari_Raajkomahan <shanksankari@gmail.com>
2023-11-08 22:16:08 +0530

    first commit

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
+const dbURI="mongodb+srv://jayasankarirr21it:jaya@cluster0.bc9sulx.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
 
-const dbID = process.env.DB_ID;
-const dbPassword = process.env.DB_PASSWORD;
-
-const connection = mongoose.connect(
-  `mongodb+srv://${dbID}:${dbPassword}@cluster0.6lt3yah.mongodb.net/?retryWrites=true&w=majority`
-);
-
-module.exports = connection;
